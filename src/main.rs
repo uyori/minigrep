98245c0441ee56cb226720b95d6ed4e5171d230e
uyori <ryo.ando1014@gmail.com>
2021-05-08 12:08:46 +0900

    refac: モジュール性とエラー処理を向上させる

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,23 @@
-use std::{env, fs::File, io::Read};
+extern crate minigrep;
+
+use std::{env, process};
+
+use minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let filename = &args[2];
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
 
-    println!("Searching for {}", query);
-    println!("In file {}", filename);
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filename);
 
-    let mut f = File::open(filename).expect("file not found");
+    if let Err(e) = minigrep::run(config) {
+        println!("Application error: {}", e);
 
-    let mut contents = String::new();
-    f.read_to_string(&mut contents)
-        .expect("something went wrong reading the file");
-
-    println!("With text:\n{}", contents);
+        process::exit(1);
+    }
 }
